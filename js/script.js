b2c7b9842014a7491268f2694997ea25f1609e14
mrk-hnr <mrk.hnr@hotmail.com>
2024-08-14 21:08:22 +0800

    All - Added responsive UI, created burger menu

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
-// let profileWidth = document.querySelectorAll(".profile-description")
-
-// window.onload = function() {
-//     for (let i = 0; i < profileWidth.length; i++) {
-//         if (profileWidth[i].offsetWidth > '150') {
-            // profileWidth[i].style.width = "180px"
-            // profileWidth[i].style.marginRight = '-25px'
-            // profileWidth[i].style.marginLeft = '-25px'
-            // profileWidth[i].children[1].style.fontSize = '18px'
-//         } 
-//         console.log(profileWidth[i].offsetWidth)
-        
-//     }
-// }
+var burgerMenu = document.getElementById('burger-menu');
+var overlay = document.getElementById('menu');
+burgerMenu.addEventListener('click',function(){
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
