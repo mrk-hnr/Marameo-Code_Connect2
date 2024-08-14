bc158992c11132a342d4798c01420a58ac5a45f7
mrk-hnr <mrk.hnr@hotmail.com>
2024-08-14 18:52:27 +0800

    Fixed #2 | Profile - Set width in order to prevent positioning break when adding new profiled

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// let profileWidth = document.querySelectorAll(".profile-description")
+
+// window.onload = function() {
+//     for (let i = 0; i < profileWidth.length; i++) {
+//         if (profileWidth[i].offsetWidth > '150') {
+            // profileWidth[i].style.width = "180px"
+            // profileWidth[i].style.marginRight = '-25px'
+            // profileWidth[i].style.marginLeft = '-25px'
+            // profileWidth[i].children[1].style.fontSize = '18px'
+//         } 
+//         console.log(profileWidth[i].offsetWidth)
+        
+//     }
+// }
