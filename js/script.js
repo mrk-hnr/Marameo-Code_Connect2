394d270ceadc22952c724e370823762c2230206c
mrk-hnr <mrk.hnr@hotmail.com>
2024-08-15 16:13:54 +0800

    Fixed #2 | Profile alignment regardless # of profile cards; names, titles, and soc links consistent alignment

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ burgerMenu.addEventListener('click',function(){
   this.classList.toggle("close");
   overlay.classList.toggle("overlay");
 });
+
+
+// let profileWidth = document.querySelectorAll(".profile-description")
+
+// window.onload = function() {
+//     for (let i = 0; i < profileWidth.length; i++) {
+//         if (profileWidth[i].offsetWidth >= '180') {
+//             profileWidth[i].style.width = "170px"
+//             // profileWidth[i].style.marginRight = '0px'
+//             // profileWidth[i].style.marginLeft = '10px'
+//             profileWidth[i].children[1].style.fontSize = '15px'
+//         } 
+//         console.log(profileWidth[i].offsetWidth)
+
+//     }
+// }
